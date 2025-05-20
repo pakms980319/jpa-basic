d0c3efb82639cd80e98e2c19e6d1685b40ae4f16
Pak MinSeok <pakms980319@gmail.com>
2025-05-20 15:02:15 +0900

    자바 ORM 표준 JPA 프로그래밍 - 기본편 > 양방향 연관관계와 연관관계의 주인 2- 주의점, 정리

--- ex1-hello-jpa/ex1-hello-jpa-start/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/ex1-hello-jpa-start/src/main/java/hellojpa/JpaMain.java
@@ -5,6 +5,8 @@ import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.Persistence;
 
+import java.util.List;
+
 public class JpaMain {
     public static void main(String[] args) {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
@@ -14,24 +16,24 @@ public class JpaMain {
         tx.begin();
 
         try {
-
             Team team = new Team();
             team.setName("TeamA");
             em.persist(team);
 
             Member member = new Member();
             member.setUsername("member1");
-            member.setTeam(team);
+            member.changeTeam(team);
             em.persist(member);
 
-            em.flush();
-            em.clear();
+//            em.flush();
+//            em.clear();
 
-            Member findMember = em.find(Member.class, member.getId());
+            Team findTeam = em.find(Team.class, team.getId());
+            List<Member> members = em.find(Team.class, team.getId()).getMembers();
+            for (Member member1 : members) {
+                System.out.println("m : " + member1);
+            }
 
-            findMember.getTeam().getMembers().forEach(member1 -> {
-                System.out.println("member = " + member1.getUsername());
-            });
 
             tx.commit();
         } catch(Exception e) {
