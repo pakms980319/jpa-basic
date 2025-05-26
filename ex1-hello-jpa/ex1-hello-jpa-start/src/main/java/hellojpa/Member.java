398c8f52214aee5caba84471518a59b2cef5a74c
Pak MinSeok <pakms980319@gmail.com>
2025-05-26 13:29:31 +0900

    자바 ORM 표준 JPA 프로그래밍 - 기본편 > 임베디드 타입

--- ex1-hello-jpa/ex1-hello-jpa-start/src/main/java/hellojpa/Member.java
+++ ex1-hello-jpa/ex1-hello-jpa-start/src/main/java/hellojpa/Member.java
@@ -2,8 +2,10 @@ package hellojpa;
 
 import jakarta.persistence.*;
 
+import java.time.LocalDateTime;
+
 @Entity
-public class Member extends BaseEntity {
+public class Member {
     @Id
     @GeneratedValue
     @Column(name = "MEMBER_ID")
@@ -12,13 +14,17 @@ public class Member extends BaseEntity {
     @Column(name = "USERNAME")
     private String username;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "TEAM_ID")
-    private Team team;
-
-
-    public Member() {
-    }
+    @Embedded
+    private Period workPeriod;
+    @Embedded
+    @AttributeOverrides({
+            @AttributeOverride(name="city", column=@Column(name = "WORK_CITY")),
+            @AttributeOverride(name="street", column=@Column(name = "WORK_STREET")),
+            @AttributeOverride(name="zipcode", column=@Column(name = "WORK_ZIPCODE"))
+    })
+    private Address workAddress;
+    @Embedded
+    private Address homeAddress;
 
     public Long getId() {
         return id;
@@ -36,11 +42,19 @@ public class Member extends BaseEntity {
         this.username = username;
     }
 
-    public Team getTeam() {
-        return team;
+    public Period getWorkPeriod() {
+        return workPeriod;
     }
 
-    public void setTeam(Team team) {
-        this.team = team;
+    public void setWorkPeriod(Period workPeriod) {
+        this.workPeriod = workPeriod;
+    }
+
+    public Address getHomeAddress() {
+        return homeAddress;
+    }
+
+    public void setHomeAddress(Address homeAddress) {
+        this.homeAddress = homeAddress;
     }
 }
