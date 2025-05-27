fa35f2249f425eaec3766f1b325fb6b3c2f2ac22
Pak MinSeok <pakms980319@gmail.com>
2025-05-27 11:34:21 +0900

    자바 ORM 표준 JPA 프로그래밍 - 기본편 > 객체지향 쿼리 언어1 - 기본 문법 > 소개

--- ex1-hello-jpa/ex1-hello-jpa-start/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/ex1-hello-jpa-start/src/main/java/hellojpa/JpaMain.java
@@ -4,6 +4,9 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.Persistence;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Root;
 
 import java.util.List;
 import java.util.Set;
@@ -17,43 +20,22 @@ public class JpaMain {
         tx.begin();
 
         try {
+
             Member member = new Member();
             member.setUsername("member1");
-            member.setHomeAddress(
-                    new Address("homeCity", "street", "10000")
-            );
-
-            member.getFavoriteFoods().add("치킨");
-            member.getFavoriteFoods().add("족발");
-            member.getFavoriteFoods().add("피자");
-
-            member.getAddressHistory().add(
-                    new AddressEntity("old1", "street", "10000")
-            );
-            member.getAddressHistory().add(
-                    new AddressEntity("old2", "street", "10000")
-            );
-
-
             em.persist(member);
 
-            em.flush();
-            em.clear();
+            // flush -> commit, query
 
-            System.out.println("===== START =====");
-            Member findMember = em.find(Member.class, member.getId());
+            // List<Member> resultList = em.createNativeQuery("select MEMBER_ID, city, street, zipcode, USERNAME from MEMBER", Member.class).getResultList();
 
-            Address a = findMember.getHomeAddress();
-            findMember.setHomeAddress(
-                    new Address("newCity", a.getStreet(), a.getZipcode())
-            );
+            // dbconn.executeQuery("select * from member");
+            // jdbc는 jpa와 관련이 없음 -> flush가 자동으로 안됨 (commit 후 flush가 될 것) -> 강제 flush가 필요
+            // em.flush();
 
-            // 치킨 -> 한식
-            findMember.getFavoriteFoods().remove("치킨");
-            findMember.getFavoriteFoods().add("한식");
-
-            findMember.getAddressHistory().remove(new AddressEntity("old1", "street", "10000"));
-            findMember.getAddressHistory().add(new AddressEntity("newCity1", "street", "10000"));
+            for (Member member1 : resultList) {
+                System.out.println("member1 = " + member1);
+            }
 
 
             tx.commit();
