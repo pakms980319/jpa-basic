e107e96ae982ad029c296d493ae7bc1c64ef8212
Pak MinSeok <pakms980319@gmail.com>
2025-05-27 16:25:23 +0900

    자바 ORM 표준 JPA 프로그래밍 - 기본편 > 객체지향 쿼리 언어1 - 기본 문법 > 조건식(CASE 등등)

--- jpql/src/main/java/jpql/JpaMain.java
+++ jpql/src/main/java/jpql/JpaMain.java
@@ -14,7 +14,7 @@ public class JpaMain {
 
         try {
             Member member = new Member();
-            member.setUsername("teamA");
+            member.setUsername("관리자");
             member.setType(MemberType.USER);
             member.setAge(10);
 
@@ -29,17 +29,12 @@ public class JpaMain {
             em.flush();
             em.clear();
 
-            String query = "select m.username, 'HELLO', true from Member m " +
-                            "where m.age between 0 and 10";
-            List<Object[]> result = em.createQuery(query, Object[].class)
-                                    .getResultList();
+            String query = "select nullif(m.username, '관리자') as username from Member m";
+            List<String> resultList = em.createQuery(query, String.class)
+                                        .getResultList();
 
-
-
-            for (Object[] objects : result) {
-                System.out.println("objects[0] = " + objects[0]);
-                System.out.println("objects[1] = " + objects[1]);
-                System.out.println("objects[2] = " + objects[2]);
+            for (String s : resultList) {
+                System.out.println("s = " + s);
             }
 
             tx.commit();
