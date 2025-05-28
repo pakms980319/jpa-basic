eab7e7d4d2975728d7454026734e3d6814e167db
Pak MinSeok <pakms980319@gmail.com>
2025-05-28 11:12:47 +0900

    자바 ORM 표준 JPA 프로그래밍 - 기본편 > 페치 조인 1 - 기본

--- jpql/src/main/java/jpql/JpaMain.java
+++ jpql/src/main/java/jpql/JpaMain.java
@@ -14,36 +14,38 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Member member = new Member();
-            member.setUsername("관리자1");
-            member.setType(MemberType.USER);
-            member.setAge(10);
+            Team teamA = new Team();
+            teamA.setName("팀A");
+            em.persist(teamA);
+
+            Team teamB = new Team();
+            teamB.setName("팀B");
+            em.persist(teamB);
+
+            Member member1 = new Member();
+            member1.setUsername("회원1");
+            member1.setTeam(teamA);
+            em.persist(member1);
 
             Member member2 = new Member();
-            member2.setUsername("관리자2");
-            member2.setType(MemberType.USER);
-            member2.setAge(10);
-
-            Team team = new Team();
-            team.setName("teamA");
-            team.getMembers().add(member);
-            team.getMembers().add(member2);
-            em.persist(team);
-
-            member.setTeam(team);
-            member2.setTeam(team);
-            em.persist(member);
+            member2.setUsername("회원2");
+            member2.setTeam(teamA);
             em.persist(member2);
+
+            Member member3 = new Member();
+            member3.setUsername("회원3");
+            member3.setTeam(teamB);
+            em.persist(member3);
 
             em.flush();
             em.clear();
 
-            String query = "select m.username from Team t join t.members m";
-            List<String> result = em.createQuery(query, String.class)
-                                        .getResultList();
+            String query = "select t from Team t join fetch t.members";
+            List<Team> result = em.createQuery(query, Team.class)
+                                    .getResultList();
 
-            for (String s : result) {
-                System.out.println("s = " + s);
+            for (Team t : result) {
+                System.out.printf("t.name : %s\nt.getMembers().size() : %s\n", t.getName(), t.getMembers().size());
             }
 
             tx.commit();
